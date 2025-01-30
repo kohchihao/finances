a8299a840b92fe098b86d68f8c0f6427db667525
Marcus Koh <kohchihao@gmail.com>
2025-01-30 22:48:43 +0800

    chore: remove tailwindcss postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import react from '@vitejs/plugin-react';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
   plugins: [react(), tsconfigPaths()],
 });
